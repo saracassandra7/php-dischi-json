2ed2546a5d4cc2e708737371797ce88fa080a0ac
saracassandra7 <saracassandra7@gmail.com>
2022-12-05 16:30:43 +0100

    [add] json converted in php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+include 'server.php'
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +12,8 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   
   <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+  <link rel="stylesheet" href="css/style.css">
   <title>Dischi Json</title>
 </head>
 <body>
--- server.php
+++ server.php
@@ -1,3 +1,11 @@
 <?php
 
+//prendo il file json
+$diskstring = file_get_contents('dischi.json');
+
+//trasformo l'oggetto json in array associativo PHP
+$disklist = json_decode($diskstring, true);
+
+var_dump($disklist);
+
 ?>
